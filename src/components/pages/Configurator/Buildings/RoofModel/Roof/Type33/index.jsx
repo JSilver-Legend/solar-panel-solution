739584f8d68b12d292358ccbda7275eb0a0e9999
JSilver-Legend <procaptain516@gmail.com>
2024-03-21 08:18:47 +0200

    add: roof cover model to type-3-3

--- src/components/pages/Configurator/Buildings/RoofModel/Roof/Type33/index.jsx
+++ src/components/pages/Configurator/Buildings/RoofModel/Roof/Type33/index.jsx
@@ -5,10 +5,12 @@ import { extrudeSetting } from 'utils/Function';
 const Type33 = ({ item, roofThickness, overHang, roofTexture }) => {
     const width = item.buildingWidth;
     const width_1 = item.buildingWidth1
-    const width_2 = item.buildingWidth2
+    const width_2 = item.buildingWidth2;
+    const width_3 = (width - width_2) / 2
     const length = item.buildingLength;
     const height = item.buildingHeight;
     const pitch = item.roofPitch;
+    const model_height = 0.2;
 
     const model = useMemo(() => {
         /**
@@ -17,7 +19,7 @@ const Type33 = ({ item, roofThickness, overHang, roofTexture }) => {
          *   w-1   |________________|    w-1
          *         |   2   /|\   2  |
          *         |______/ | \_____|
-         *               |  |  |
+         *           w-3 |  |  | w-3
          *               |  |  |
          *               |3 | 4|
          *               |__|__|
@@ -82,30 +84,154 @@ const Type33 = ({ item, roofThickness, overHang, roofTexture }) => {
             angle1: angle1,
             angle2: angle2,
         }
+    }, [length, pitch, width, width_1, width_2]);
+
+    const coverModel = useMemo(() => {
+
+        
+
+        const model_1_base = new THREE.Shape()
+        model_1_base.moveTo( -width / 2, 0);
+        model_1_base.lineTo(  width / 2, 0 );
+        model_1_base.lineTo(  width / 2, -model_height );
+        model_1_base.lineTo( -width / 2, -model_height );
+        model_1_base.closePath();
+
+        const model_2_base = new THREE.Shape();
+        model_2_base.moveTo( 0, 0);
+        model_2_base.lineTo( width_3, 0 );
+        model_2_base.lineTo( width_3, -model_height );
+        model_2_base.lineTo( 0, -model_height );
+        model_2_base.closePath();
+
+        const model_3_base = new THREE.Shape()
+        model_3_base.moveTo(  length / 2 - width_1, 0);
+        model_3_base.lineTo( -length / 2, 0);
+        model_3_base.lineTo( -length / 2, -model_height );
+        model_3_base.lineTo(  length / 2 - width_1, -model_height );
+        model_3_base.closePath();
+
+        // const model_3_base = new THREE.Shape()
+        // model_3_base.moveTo(  length / 2, 0);
+        // model_3_base.lineTo( -length / 2, 0);
+        // model_3_base.lineTo( -length / 2, -model_height );
+        // model_3_base.lineTo(  length / 2, -model_height );
+        // model_3_base.closePath();
+
+        // const model_4_base = new THREE.Shape()
+        // model_4_base.moveTo( -width / 2, 0);
+        // model_4_base.lineTo(  width / 2, 0 );
+        // model_4_base.lineTo(  width / 2, -model_height );
+        // model_4_base.lineTo( -width / 2, -model_height );
+        // model_4_base.closePath();
+
+        // const model_1_slope = new THREE.Shape();
+        // model_1_slope.moveTo( 0, model_height );
+        // model_1_slope.lineTo( 0, 0 );
+        // model_1_slope.lineTo( -width_1 / 2, pitch );
+        // model_1_slope.lineTo( -width_1 / 2, pitch + model_height);
+        // model_1_slope.closePath();
+
+        // const model_2_slope = new THREE.Shape();
+        // model_2_slope.moveTo( 0, 0 );
+        // model_2_slope.lineTo( 0, model_height );
+        // model_2_slope.lineTo( width_2 / 2, pitch + model_height );
+        // model_2_slope.lineTo( width_2 / 2, pitch );
+        // model_2_slope.closePath();
+        
+        // const model_3_slope = new THREE.Shape();
+        // model_3_slope.moveTo( 0, 0 );
+        // model_3_slope.lineTo( 0, model_height );
+        // model_3_slope.lineTo( width_1 / 2, pitch + model_height  );
+        // model_3_slope.lineTo( width_1 / 2, pitch );
+        // model_3_slope.closePath();
+        
+        // const model_4_slope = new THREE.Shape();
+        // model_4_slope.moveTo( 0, model_height );
+        // model_4_slope.lineTo( 0, 0 );
+        // model_4_slope.lineTo( -width_2 / 2, pitch  );
+        // model_4_slope.lineTo( -width_2 / 2, pitch + model_height);
+        // model_4_slope.closePath();
+
+        return{
+            model_1_base: model_1_base,
+            model_2_base: model_2_base
+            // model_2_base: model_2_base,
+            // model_3_base: model_3_base,
+            // model_4_base: model_4_base,
+            // model_1_slope: model_1_slope,
+            // model_2_slope: model_2_slope,
+            // model_3_slope: model_3_slope,
+            // model_4_slope: model_4_slope
+        }
     }, [length, pitch, width, width_1, width_2])
 
     return (
         <group>
-            <mesh name='roof-model-3-3-1' position={[0, height, -length / 2]} rotation={[model.angle1, 0, 0]} scale={[1, 1, 0.1]}>
-                <extrudeGeometry args={[model.model1, extrudeSetting(roofThickness)]} />
-                <meshStandardMaterial
-                    side={THREE.DoubleSide}
-                    color={'#61980A'}
-                    transparent
-                    metalness={0.5}
-                    roughness={0.5}
-                />
-            </mesh>
-            <mesh name='roof-model-3-3-2' position={[0, height, -length / 2 + width_1]} rotation={[-model.angle1, 0, 0]} scale={[1, 1, 0.1]}>
-                <extrudeGeometry args={[model.model2, extrudeSetting(roofThickness)]} />
-                <meshStandardMaterial
-                    side={THREE.DoubleSide}
-                    color={'#61980A'}
-                    transparent
-                    metalness={0.5}
-                    roughness={0.5}
-                />
-            </mesh>
+            <group name='roof_model_1'>
+                <group>
+                    <mesh name='roof-model-3-3-1' position={[0, height, -length / 2]} rotation={[model.angle1, 0, 0]} scale={[1, 1, 0.1]}>
+                        <extrudeGeometry args={[model.model1, extrudeSetting(roofThickness)]} />
+                        <meshStandardMaterial
+                            side={THREE.DoubleSide}
+                            color={'#61980A'}
+                            transparent
+                            metalness={0.5}
+                            roughness={0.5}
+                        />
+                    </mesh>
+                </group>
+                <group>
+                    <mesh name='model_1_base' position={[0, height, -length / 2]} scale={[1, 1, 0.1]}>
+                        <extrudeGeometry args={[coverModel.model_1_base, extrudeSetting(roofThickness)]} />
+                        <meshStandardMaterial
+                            side={THREE.DoubleSide}
+                            color={'#61980A'}
+                            transparent
+                            metalness={0.5}
+                            roughness={0.5}
+                        />
+                    </mesh>
+                </group>
+            </group>
+            <group name='roof_model_2'>
+                <group>
+                    <mesh name='roof-model-3-3-2' position={[0, height, -length / 2 + width_1]} rotation={[-model.angle1, 0, 0]} scale={[1, 1, 0.1]}>
+                        <extrudeGeometry args={[model.model2, extrudeSetting(roofThickness)]} />
+                        <meshStandardMaterial
+                            side={THREE.DoubleSide}
+                            color={'#61980A'}
+                            transparent
+                            metalness={0.5}
+                            roughness={0.5}
+                        />
+                    </mesh>
+                </group>
+                <group>
+                    <mesh name='model_2_left_base' position={[-width/2, height, -(length /2 - width_1)]} scale={[1, 1, 0.1]}>
+                        <extrudeGeometry args={[coverModel.model_2_base, extrudeSetting(roofThickness)]} />
+                        <meshStandardMaterial
+                            side={THREE.DoubleSide}
+                            color={'white'}
+                            transparent
+                            metalness={0.5}
+                            roughness={0.5}
+                        />
+                    </mesh>
+                </group>
+                <group>
+                    <mesh name='model_2_right_base' position={[width / 2 - width_3, height, -(length /2 - width_1)]}  scale={[1, 1, 0.1]}>
+                        <extrudeGeometry args={[coverModel.model_2_base, extrudeSetting(roofThickness)]} />
+                        <meshStandardMaterial
+                            side={THREE.DoubleSide}
+                            color={'white'}
+                            transparent
+                            metalness={0.5}
+                            roughness={0.5}
+                        />
+                    </mesh>
+                </group>
+            </group>
             <group position={[-width_2 / 2, height, 0]} rotation={[0, 0, model.angle2]}>
                 <mesh name='roof-model-3-3-3' rotation={[-Math.PI / 2, 0, 0]} scale={[1, 1, 0.1]}>
                     <extrudeGeometry args={[model.model3, extrudeSetting(roofThickness)]} />
